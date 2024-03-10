1558b4208cc9deab8d86769bb92a1638737baa78
Ajeya5999 <ajeyasengar@gmail.com>
2024-03-10 14:18:45 +0530

    Fourth Commit: Added the functionality to delete a product by ID

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -7,7 +7,14 @@ module.exports.create = async function(req, res) {
             name: name,
             quantity: quantity
         });
-        res.status(201).json(data);
+        res.status(201).json({
+            "data": {
+                "product": {
+                    "name": data.name,
+                    "quantity": data.quantity 
+                }
+            }
+        });
     } catch(err) {
         console.log('error', err);
         res.status(500).json({ message: err.message });
@@ -19,7 +26,26 @@ module.exports.getProducts = async function(req, res) {
         let data = await Product.find({})
         .select('_id name quantity')
         .exec();
-        res.status(200).json(data);
+        res.status(200).json({
+            "data": {
+                "product": data
+            }
+        });
+    } catch(err) {
+        console.log('error', err);
+        res.status(500).json({ message: err.message });
+    }
+}
+
+module.exports.delete = async function(req, res) {
+    let productId = req.params.id;
+    try {
+        await Product.findByIdAndDelete(productId);
+        res.status(200).json({
+            "data": {
+                "message": "product deleted"
+            }
+        })
     } catch(err) {
         console.log('error', err);
         res.status(500).json({ message: err.message });
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ console.log("Router has been loaded");
 
 router.get('/products', productController.getProducts); //Get the list of all products
 router.post('/products/create', productController.create); //Add a product to the list
+router.post('/products/:id', productController.delete) //Delete a product from the list
 // router.all('*', homeController.notFound); //For Non Existant Routes  
 
 module.exports = router;
