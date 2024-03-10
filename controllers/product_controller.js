d9937a333a18a069d62b6cf80b0316546202cf18
Ajeya5999 <ajeyasengar@gmail.com>
2024-03-10 14:43:07 +0530

    Fifth Commit: Added the functionality to update a product quantity by ID

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -51,3 +51,29 @@ module.exports.delete = async function(req, res) {
         res.status(500).json({ message: err.message });
     }
 }
+
+module.exports.update = async function(req, res) {
+    let productId = req.params.id, number = req.query.number;
+    try {
+        let data = await Product.findByIdAndUpdate(productId, {
+            $inc: {
+                quantity: number
+            }
+        }, {
+            returnDocument: 'after'
+        });
+        res.status(200).json({
+            "data": {
+                "product": {
+                    "_id": data._id,
+                    "name": data.name,
+                    "quantity": data.quantity
+                },
+                "message": "updated successfully"
+            }
+        });
+    } catch(err) {
+        console.log('error', err);
+        res.status(500).json({ message: err.message });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ console.log("Router has been loaded");
 router.get('/products', productController.getProducts); //Get the list of all products
 router.post('/products/create', productController.create); //Add a product to the list
 router.post('/products/:id', productController.delete) //Delete a product from the list
+router.post('/products/:id/update_quantity', productController.update) //Update a product's quantity from the list
 // router.all('*', homeController.notFound); //For Non Existant Routes  
 
 module.exports = router;
