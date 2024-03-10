9534b44e82f52a4b7a73c3381297624f77ddf62a
Ajeya5999 <ajeyasengar@gmail.com>
2024-03-10 15:53:04 +0530

    Seventh Commit: rearranged and commented code

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -8,11 +8,9 @@ module.exports.create = async function(req, res) {
             quantity: quantity
         });
         res.status(201).json({
-            "data": {
-                "product": {
-                    "name": data.name,
-                    "quantity": data.quantity 
-                }
+            "product": {
+                "name": data.name,
+                "quantity": data.quantity 
             }
         });
     } catch(err) {
@@ -26,11 +24,7 @@ module.exports.getProducts = async function(req, res) {
         let data = await Product.find({})
         .select('_id name quantity')
         .exec();
-        res.status(200).json({
-            "data": {
-                "product": data
-            }
-        });
+        res.status(200).json({ "product": data });
     } catch(err) {
         console.log('error', err);
         res.status(500).json({ message: err.message });
@@ -41,11 +35,7 @@ module.exports.delete = async function(req, res) {
     let productId = req.params.id;
     try {
         await Product.findByIdAndDelete(productId);
-        res.status(200).json({
-            "data": {
-                "message": "product deleted"
-            }
-        })
+        res.status(200).json({ "message": "product deleted"});
     } catch(err) {
         console.log('error', err);
         res.status(500).json({ message: err.message });
@@ -63,14 +53,12 @@ module.exports.update = async function(req, res) {
             returnDocument: 'after'
         });
         res.status(200).json({
-            "data": {
-                "product": {
-                    "_id": data._id,
-                    "name": data.name,
-                    "quantity": data.quantity
-                },
-                "message": "updated successfully"
-            }
+            "product": {
+                "_id": data._id,
+                "name": data.name,
+                "quantity": data.quantity
+            },
+            "message": "updated successfully"
         });
     } catch(err) {
         console.log('error', err);
--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ console.log("Router has been loaded");
 
 router.get('/products', productController.getProducts); //Get the list of all products
 router.post('/products/create', productController.create); //Add a product to the list
-router.post('/products/:id', productController.delete) //Delete a product from the list
-router.post('/products/:id/update_quantity', productController.update) //Update a product's quantity from the list
+router.delete('/products/:id', productController.delete) //Delete a product from the list
+router.put('/products/:id/update_quantity', productController.update) //Update a product's quantity from the list
 router.all('*', productController.notFound); //For Non Existant Routes  
 
 module.exports = router;
